38e60e4e3f9877cbdeada393f0db82c93815a0a8
Arjane Lolong <rjn.sachi@gmail.com>
2020-08-01 09:51:29 +0800

    refactor: get random image from docker.demo.randomimageurl

--- src/index.js
+++ src/index.js
@@ -1,25 +1,24 @@
 const http = require('http');
-const coolImages = require('cool-images');
+const fetch = require('node-fetch');
 var sunTzu = require('sun-tzu-quotes')
 
 const hostname = '0.0.0.0';
 const port = 9110;
 
+let imageUrl = '';
+async function setImageUrl(){
+    const response = await fetch('http://docker.demo.randomimageurl:6110/');
+    const result = await response.text();
+    imageUrl = result;
+}
+setImageUrl();
+
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html; charset=UTF-8');
-  res.end(`<center><img src="${coolImages.one(600, 800)}"><h3>${sunTzu()}</h3></center>`);
+  res.end(`<h3>${sunTzu()}</h3><br /> <img src="${imageUrl}">`);
 });
-
-
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
-
-var count = 0;
-function start(){
-    console.log(count++);
-    setTimeout(start, 1000);
-}
-start();
